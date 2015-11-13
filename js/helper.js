ee4ef5e58b081d5c85ffda73108040433c814ee2
HDGuda <dgeller@niva.de>
2015-11-14 00:37:39 +0100

    For-in-Loop eingebaut

--- js/helper.js
+++ js/helper.js
@@ -4,7 +4,7 @@ These are HTML strings. As part of the course, you'll be using JavaScript functi
 replace the %data% placeholder text you see in them.
 */
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr/>';
+var HTMLheaderRole = '<span class="white-lean">%data%</span><hr/>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
@@ -20,7 +20,7 @@ var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
 var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
 var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
 
-var HTMLworkStart = '<div class="work-entry"></div>';
+var HTMLworkStart = '<div class="work-entry" id="work-entry"></div>';
 var HTMLworkEmployer = '<a href="#">%data%';
 var HTMLworkTitle = ' - %data%</a>';
 var HTMLworkDates = '<div class="date-text">%data%</div>';
@@ -33,7 +33,7 @@ var HTMLprojectDates = '<div class="date-text">%data%</div>';
 var HTMLprojectDescription = '<p><br>%data%</p>';
 var HTMLprojectImage = '<img src="%data%">';
 
-var HTMLschoolStart = '<div class="education-entry"></div>';
+var HTMLschoolStart = '<div class="education-entry" id="education-entry"></div>';
 var HTMLschoolName = '<a href="#">%data%';
 var HTMLschoolDegree = ' -- %data%</a>';
 var HTMLschoolDates = '<div class="date-text">%data%</div>';
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,99 @@
-var name = "John M. Keynes";
-var formattedName = HTMLheaderName.replace("%data%", name);
-var role = "Dead Economist";
-var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+var skills = ["Analytical mind", "Creative thinker", "Handsome guy", "Clever rhetorician", "Unfortunately dead"];
+var bio = {
+	"name": "John M. Keynes",
+	"role": "Dead Economist",
+	"contact": ["Downs above Tilton", "Please don't call", "Surely no email", "Who needs Twitter?", "Github?", "Read my books!"],
+	"pictureURL": "https://de.wikipedia.org/wiki/John_Maynard_Keynes#/media/File:Keynes_1933.jpg",
+	"welcome-msg": "In the long run we are all dead.",
+	"skills": skills,
+	"biodates": ["5 June 1883", "21 April 1946"]
+};
+
+var work = {
+	"career": [
+		{"employer": "British government",
+	 	"title": "Financial represantive of the Treasury",
+	 	"dates": "1915 - 1919",
+	 	"location": "London",
+	 	"description": "Participated in the Versailles peace conference."
+		},
+		{"employer": "Self-employed writer",
+	 	"title": "Author",
+	 	"dates": "1919 - 1924",
+	 	"location": "Cambridge",
+	 	"description": "Published 'The economic consequences of the Peace', 'A Treatise on Probability' and other well-received books."
+		},
+		{"employer": "University of Cambridge",
+		 "title": "Professor",
+		 "dates": "1924 - 1938",
+		 "location": "Cambridge",
+		 "description": "Had to deal with too many stupid, bored students."
+		}
+	]
+};
+
+var education = {
+	"schools": [
+	{"name": "Cambridge College",
+	 "city": "Cambridge, UK",
+	 "degree": "B.A.",
+	 "majors": ["Fine Arts", "Macroeconomics"],
+	 "yearsattended": "1901 - 1903"
+
+	},
+	{"name": "Cambridge University",
+	 "city": "Cambridge, UK",
+	 "degree": "M.Sc. in Economics",
+	 "majors": "Monetary theory and policy",
+     "yearsattended": "1903 - 1904"
+	}
+	]
+};
+
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+if (typeof(bio.skills) !== "undefined") {
+	$("#header").append(HTMLskillsStart);
+
+};
+
+
+var formattedMobile = HTMLmobile.replace("%data%", bio.contact[1]);
+var formattedLocation = HTMLlocation.replace("%data%", bio.contact[0]);
+var formattedEmail = HTMLemail.replace("%data%", bio.contact[2]);
+var formattedTwitter = HTMLtwitter.replace("%data%", bio.contact[3]);
+var formattedGithub = HTMLgithub.replace("%data%", bio.contact[4]);
+var formattedBlog = HTMLblog.replace("%data%", bio.contact[5]);
+
+$("#topContacts").append(formattedMobile);
+$("#topContacts").append(formattedEmail);
+$("#topContacts").append(formattedTwitter);
+$("#topContacts").append(formattedGithub);
+$("#topContacts").append(formattedBlog);
+$("#topContacts").append(formattedLocation);
+
+for (job in work.career) {
+	var formattedEmployerTitle = HTMLworkEmployer.replace("%data%", work.career[job].employer) + HTMLworkTitle.replace("%data%", work.career[job].title);
+	var formattedworkDates = HTMLworkDates.replace("%data%", work.career[job].dates);
+	var formattedworkLocation = HTMLworkLocation.replace("%data%", work.career[job].location);
+	var formattedworkDescription = HTMLworkDescription.replace("%data%", work.career[job].description);
+	$("#workExperience").append(HTMLworkStart);
+	$("#work-entry").append(formattedEmployerTitle);
+	$("#work-entry").append(formattedworkDates);
+	$("#work-entry").append(formattedworkLocation);
+	$("#work-entry").append(formattedworkDescription);
+};
+
+
+$("#education").append(HTMLschoolStart);
+$("#education-entry").append(formattedEducation);
+$("#education-entry").append(formattedDegree);
+
+$("#footerContacts").append(formattedGithub);
+$("#footerContacts").append(formattedBlog);
+$("#footerContacts").append(formattedLocation);
